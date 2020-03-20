fae8646d490f37472d63ca90c315eee853a3851e
HWYlove <hsearoy.gmail.com>
2020-03-20 23:28:07 +0800

    第一次提交——初始化项目

---
+++ app/src/main/java/com/cpersimmon/advanced/AuthManager.java
@@ -0,0 +1,131 @@
+package com.cpersimmon.advanced;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Switch;
+import android.widget.TextView;
+
+import com.cpersimmon.advanced.Utils.ShellUtils;
+
+import static com.cpersimmon.advanced.Utils.toWork.tryCreate;
+import static com.cpersimmon.advanced.Utils.toWork.tryDelete;
+import static com.cpersimmon.advanced.Utils.toWork.通用Switch点击操作;
+import static com.cpersimmon.advanced.Utils.toWork.isExist;
+
+public class AuthManager extends AppCompatActivity implements View.OnClickListener{
+    TextView t1;
+    TextView t2;
+    Switch s1;
+    Switch s2;
+    Button b1;
+    Button b2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_auth_manager);
+        t1=findViewById(R.id.text_1);
+        t2=findViewById(R.id.text_2);
+        s1=findViewById(R.id.switch_1);
+        s2=findViewById(R.id.switch_2);
+        b1=findViewById(R.id.button_1);
+        b2=findViewById(R.id.button_2);
+        t1.setText("取消读取手机权限");
+        t2.setText("取消读写存储空间");
+        s1.setChecked(!isExist("501"));
+        s1.setOnClickListener(this);
+        s2.setChecked(!isExist("502"));
+        s2.setOnClickListener(this);
+        b1.setOnClickListener(this);
+        b2.setOnClickListener(this);
+
+        android.support.v7.app.ActionBar actionBar = getSupportActionBar();
+        if(actionBar != null) {
+            actionBar.setHomeButtonEnabled(true);
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish(); // back button
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View view){
+        switch (view.getId()){
+            case R.id.switch_1:
+                通用Switch点击操作(this,s1,"501");
+                //生效问题处理();
+            break;
+            case R.id.switch_2:
+                通用Switch点击操作(this,s2,"502");
+                //生效问题处理();
+            break;
+            case  R.id.button_1:
+                生效问题处理(1);
+            break;
+            case R.id.button_2:
+                生效问题处理(2);
+            break;
+        }
+    }
+    private boolean 生效问题处理(int i){
+        ShellUtils su=new ShellUtils();
+        //ShellUtils.CommandResult Result=new ShellUtils.CommandResult();
+        String str="am kill";
+        String str_0="pm disable";
+        String str_1="pm enable";
+        String str1=" ";
+        String str2="com.miui.securitycenter";
+        String str3="com.lbe.security.miui";
+        String s1=str+str1+str2;
+        switch (i){
+            case 1:ShellUtils.CommandResult Result1=su.execCommand(s1,true,true);
+
+                return (Result1.result==0);
+            case 2:
+                boolean b=isExist("403");
+                if(b){
+                    tryDelete("403");
+                }
+                ShellUtils.CommandResult Result2=su.execCommand(str_0+str1+str3+"\n"+str_1+str1+str3,true,true);
+                //su.execCommand(str_1+str1+str3,true,true);
+                if(b){
+                    tryCreate("403");
+                }
+                return (Result2.result==0);
+            default:
+                return false;
+        }
+        //ShellUtils.CommandResult Result=su.execCommand("pm enable com.x7890.shortcutcreator",true,true);
+        /*
+        ShellUtils.CommandResult Result=su.execCommand(s1,true,true);
+        try {
+            Thread.sleep(100);
+        }catch (Exception e){
+            Log.e("重要",e.toString());
+        }
+        ShellUtils.CommandResult Result1=su.execCommand(s2,true,true);
+        Log.e("重要信息1",String.valueOf(Result1.result));
+        Log.e("重要信息2",Result1.errorMsg);
+        Log.e("重要信息3",Result1.successMsg);
+        return (Result1.result==0)&&(Result1.result==0);
+        */
+        /*
+        if(Result.result==0)
+            return true;
+        else
+            return false;
+            */
+    }
+
+}
---
+++ app/src/main/java/com/cpersimmon/advanced/MainActivity.java
@@ -0,0 +1,166 @@
+package com.cpersimmon.advanced;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.maiml.library.BaseItemLayout;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.cpersimmon.advanced.Utils.toWork.isExist;
+import static com.cpersimmon.advanced.Utils.toWork.说明;
+import static com.cpersimmon.advanced.Utils.toWork.通用Switch点击操作;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    Switch aSwitch_101;
+    Switch aSwitch_201;
+    Switch aSwitch_301;
+    Switch aSwitch_305;
+    Switch aSwitch_401;
+    Switch aSwitch_402;
+    Switch aSwitch_403;
+    Switch aSwitch_404;
+    TextView name_305;
+    TextView name_305_1;
+    BaseItemLayout layout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        checkPermission();
+        说明(this);
+        layout= findViewById(R.id.layout);
+        List<String> valueList = new ArrayList<>();
+        valueList.add("授权管理：去除权限");
+        List<Integer> resIdList = new ArrayList<>();
+        resIdList.add(R.mipmap.ic_launcher);
+        layout.setValueList(valueList) // 文字 list
+                .setResIdList(resIdList) // icon list
+                .setArrowResId(R.drawable.right_arrow_black)// 右边的箭头
+                .setArrowIsShow(true) //是否显示右边的箭头
+                .setItemMarginTop(18)  //设置 item的边距（全部）
+                //.setItemMarginTop(10,10) // 设置 某一个item 的边距
+                .setIconHeight(30)    // icon 的高度
+                .setIconWidth(30)      // icon 的宽度
+                .create();
+        layout.setOnBaseItemClick(new BaseItemLayout.OnBaseItemClick() {
+            @Override
+            public void onItemClick(int position) {
+                switch (position){
+                    case 0:
+                        Intent intent = new Intent   (MainActivity.this,AuthManager.class);
+                        startActivity(intent);
+                        break;
+                }
+            }
+        });
+
+
+
+        //分离符号
+        aSwitch_101=Switch初始化(R.id.switch_101,"101");
+        aSwitch_201=Switch初始化(R.id.switch_201,"201");
+        aSwitch_301=Switch初始化(R.id.switch_301,"301");
+        aSwitch_305=Switch初始化(R.id.switch_305,"305");
+        aSwitch_401=Switch初始化(R.id.switch_401,"401");
+        aSwitch_402=Switch初始化(R.id.switch_402,"402");
+        aSwitch_403=Switch初始化(R.id.switch_403,"403");
+        aSwitch_404=Switch初始化(R.id.switch_404,"404");
+        name_305=findViewById(R.id.name_305);
+        name_305_1=findViewById(R.id.name_305_1);
+        name_305.setOnClickListener(this);
+        android.support.v7.app.ActionBar actionBar = getSupportActionBar();
+        if(actionBar != null) {
+            actionBar.setHomeButtonEnabled(true);
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish(); // back button
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    private Switch Switch初始化(int id, String name){
+        Switch aSwitch = findViewById(id);
+        aSwitch.setChecked(!isExist(name));
+        aSwitch.setOnClickListener(this);
+        return aSwitch;
+    }
+    @Override
+    public void onClick(View view){
+        switch (view.getId()){
+            case R.id.switch_101:
+                通用Switch点击操作(this,aSwitch_101,"101");
+                break;
+            case R.id.switch_201:
+                通用Switch点击操作(this,aSwitch_201,"201");
+                break;
+            case R.id.switch_301:
+                通用Switch点击操作(this,aSwitch_301,"301");
+                break;
+            case R.id.switch_305:
+                通用Switch点击操作(this,aSwitch_305,"305");
+                break;
+            case R.id.switch_401:
+                通用Switch点击操作(this,aSwitch_401,"401");
+                break;
+            case R.id.switch_402:
+                通用Switch点击操作(this,aSwitch_402,"402");
+                break;
+            case R.id.switch_403:
+                通用Switch点击操作(this,aSwitch_403,"403");
+                break;
+            case R.id.switch_404:
+                通用Switch点击操作(this,aSwitch_404,"404");
+                break;
+            case R.id.name_305:
+                if(name_305_1.isShown()){
+                    name_305_1.setVisibility(View.GONE);
+                }else{
+                    name_305_1.setVisibility(View.VISIBLE);
+                }
+                break;
+        }
+    }
+
+    /*
+    private void 通用Switch点击操作(Switch aSwitch,String name){
+        if(aSwitch.isChecked()) {
+            if(!tryDelete(name)){
+                Toast.makeText(this, "操作失败", Toast.LENGTH_SHORT).show();
+            }
+        } else{
+            if(!tryCreate(name)){
+                Toast.makeText(this, "操作失败", Toast.LENGTH_SHORT).show();
+            }
+        }
+        aSwitch.setChecked(!isExist(name));
+    }
+    */
+
+    private void checkPermission(){
+        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+            if(ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+                Toast.makeText(this, "本软件需要读写外部储存才能使用", Toast.LENGTH_SHORT).show();
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+            }
+        }
+    }
+}
