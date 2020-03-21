010c79c8e68a232391e84ef09c7771c91e05b037
HWYlove <hsearoy.gmail.com>
2020-03-21 10:39:20 +0800

    尝试提交git库（2020.3.21）

---
+++ app/src/main/java/com/cpersimmon/advanced/Utils/FreezeApp.java
@@ -0,0 +1,45 @@
+package com.cpersimmon.advanced.Utils;
+
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.support.v7.app.AppCompatActivity;
+
+public class FreezeApp extends AppCompatActivity {
+    private Context ct_sc1;
+    private int i_sc1;
+    private String name_sc1;
+    public FreezeApp(Context ct_sc0){
+        ct_sc1=ct_sc0;
+    }
+    //0是解冻，1是冻结
+    public boolean freeze(int i_sc0,String name_sc0){
+        i_sc1=i_sc0;
+        name_sc1=name_sc0;
+        int check;
+        try{
+            PackageManager pm=ct_sc1.getPackageManager();
+            if(i_sc1==0){
+                pm.setApplicationEnabledSetting(name_sc1, PackageManager.COMPONENT_ENABLED_STATE_DEFAULT, 0);
+                check=pm.getApplicationEnabledSetting(name_sc1);
+                if(check==0||check==1){
+                    return true;
+                }
+                else
+                    return false;
+            }
+            else {
+                pm.setApplicationEnabledSetting(name_sc1, PackageManager.COMPONENT_ENABLED_STATE_DISABLED, 0);
+                check=pm.getApplicationEnabledSetting(name_sc1);
+                if(check==0||check==1){
+                    return false;
+                }
+                else
+                    return true;
+            }
+
+        }catch (Exception e){
+            return false;
+        }
+    }
+}
+
